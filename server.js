5c9131b57e81b399257007c9d912020164550003
taha-yasin-saad <taha.yasin.saad@gmail.com>
2021-11-14 02:10:34 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+const bodyParser = require('body-parser');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 3000;
+// Setup Server
+const server = app.listen(port, () => { console.log(`running on localhost:  + ${port}`); });
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+app.get('/all', (req,res) => {
+    res.send(projectData);
+});
+// Post Route
+
---
+++ website/app.js
@@ -0,0 +1,53 @@
+/* Global Variables */
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'f9c9b0545062a589752704f7909886d1';
+
+
+
+/* Function called by event listener */
+/* Function to GET Web API Data*/
+// let zipCode = '94040';
+
+
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+// Event listener to add function to existing HTML DOM element
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    let zipCode = document.getElementById('zip').value;
+    let feelings = document.getElementById('feelings').value;
+
+    let Data = retrieveData(baseURL, zipCode, apiKey);
+
+    Data.then(function (result) {
+        console.log(result.main.temp) // "Some User token"
+        document.getElementById('temp').innerHTML = result.main.temp;
+        document.getElementById('date').innerHTML = newDate;
+        document.getElementById('content').innerHTML = feelings;
+    })
+
+
+}
+
+/* Function to POST data */
+const retrieveData = async (url, zip, Key) => {
+
+    const res = await fetch(url + zip + '&appid=' + Key)
+    try {
+
+        const data = await res.json();
+        console.log(data)
+
+        return data;
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+/* Function to GET Project Data */
